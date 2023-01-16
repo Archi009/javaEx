d18fe6452e60ad4d4cc563aa0c4175ac4784a61d
Seonguk. Lim <dnrdl6817@gmail.com>
2023-01-16 17:52:21 +0900

    111

--- homework/src/homework/homework20230112.java
+++ homework/src/homework/homework20230112.java
@@ -14,6 +14,46 @@ public class homework20230112 {
 	    int selectNm = Integer.parseInt(sc.nextLine());
 	          return selectNm;
 	}
+	static int prucNm = 0;
+	public static void countProduc() {
+		System.out.println("상품수를 입력하시오>>");
+	    prucNm = Integer.parseInt(sc.nextLine());			
+		
+	}
+	public static void insert () {
+		prucAry = new Product[prucNm];
+		for(int i = 0; i<prucAry.length; i++) {
+			prucAry[i] = new Product();
+			System.out.println("상품 명을 입력하시오>>");
+			prucAry[i].name = sc.nextLine();
+			System.out.println("상품 가격을 입력하시오>>");
+			prucAry[i].price = Integer.parseInt(sc.nextLine());	
+			}
+	}
+	public static void eachPrice () {
+		for(Product check:prucAry) {
+			check.getInfo();
+		}		
+	}
+	public static void analyze() {
+		int total = 0; 
+		int max = 0;
+		String expenPro = null;
+		for (int i = 0; i < prucAry.length; i++) {
+			//비교 기준값을 정해줌
+			max = prucAry[0].price;
+			//최댓값
+			if(max<prucAry[i].price) {
+				max=prucAry[i].price;
+				expenPro=prucAry[i].name;
+			}
+			total += prucAry[i].price;
+		}
+		System.out.println("최고가격 제품:"+ expenPro + "을 제외한 가격의 총합"+(total-max)+"원");
+	}
+	public static void systemOff() {
+		System.out.println("프로그램 종료");	
+	}
 	public static void main(String[] args) {
 		// 문제2) 다음은 키보드로부터 상품 수와 상품 가격을 입력받아서
 		// 최고 가격을 가지는 제품과 해당 제품을 제외한 제품들의 총 합을 구하는 프로그램을 작성하세요.
@@ -24,60 +64,27 @@ public class homework20230112 {
 		// 출력예시, "상품명 : 가격"
 		// 4) 분석기능은 최고 가격을 가지는 제품과 해당 제품을 제외한 제품들의 총합을 구합니다.
 		// 5) 종료 시에는 프로그램을 종료한다고 메세지를 출력하도록 구현하세요.
-		int prucNm = 0;
 		
 		while(true) {			
 			 int selectNm = menuSelect();
 			if (selectNm == 1) {
 				//상품수
-				System.out.println("상품수를 입력하시오>>");
-				prucNm = Integer.parseInt(sc.nextLine());				
+				countProduc();	
 				
 			}else if (selectNm == 2) {
 				//상품 및 가격 입력
-				prucAry = new Product[prucNm];
-				for(int i = 0; i<prucAry.length; i++) {
-					prucAry[i] = new Product();
-					System.out.println("상품 명을 입력하시오>>");
-					prucAry[i].name = sc.nextLine();
-					System.out.println("상품 가격을 입력하시오>>");
-					prucAry[i].price = Integer.parseInt(sc.nextLine());	
-				}				
-				
+				insert ();							
+			
 			}else if (selectNm == 3) {
 				//제품별 가격
-				for(Product check:prucAry) {
-					check.getInfo();
-				}
-				
+				eachPrice();				
 			}else if (selectNm == 4) {
 				//분석
-				int total = 0; 
-				int max = 0;
-				String expenPro = null;
-				for (int i = 0; i < prucAry.length; i++) {
-					//비교 기준값을 정해줌
-					max = prucAry[0].price;
-					//최댓값
-					if(max<prucAry[i].price) {
-						max=prucAry[i].price;
-						expenPro=prucAry[i].name;
-					}
-					total += prucAry[i].price;
-				}
-				System.out.println("최고가격 제품:"+ expenPro + "을 제외한 가격의 총합"+(total-max)+"원");
-					
+				analyze();					
 			}else if (selectNm == 5) {
 				//종료
-				System.out.println("프로그램 종료");
 				break;
 			}
-			
-			
-			
-			
-			
-			
 			
 		}
 		
